3268bd1c3bd9242e2d7b6709a652129be7acc832
shivangyadav9357 <shivangyadav9357@gmail.com>
2023-11-01 03:05:02 +0530

    Change logo and favicon

--- src/components/common/logo/TextLogo.tsx
+++ src/components/common/logo/TextLogo.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import logoTextLight from "../../../assets/img/fixedFiTextLight.png";
 import logoTextDark from "../../../assets/img/fixedFiTextDark.png";
-import flatwhite from "../../../assets/img/fixedFiLogoDark.png";
+import flatwhite from "../../../assets/img/adao-logo.png";
 import flatblack from "../../../assets/img/fixedFiLogo.png";
 import "./logo.css";
 import { useRecoilValue } from "recoil";
 import { themeState } from "../../../context/themeState";
+import { Text } from "@chakra-ui/react";
 
 const TextLogo = () => {
   const theme = useRecoilValue(themeState);
   return (
     <div className="text-logo-container">
-      <img className="text-logo-img" src={theme === "Dark"?flatblack:flatwhite} />
-      <img className="text-logo-title" src={theme === "Dark"?logoTextLight:logoTextDark} />
+      <img className="text-logo-img" src={flatwhite} />
+      <Text fontSize={"1.5rem"}>Interchain DAOs</Text>
       {/* <h3>StakeEasy 2.0</h3> */}
     </div>
   );
